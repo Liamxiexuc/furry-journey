93a5708f9509ea1d62856483e79d32545b4b8095
JimHui12 <jianghua.hui@uq.net.au>
2019-11-20 10:32:35 +1000

    App.js updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Routes from './route/Routes';
+import TopNav from './navigation/TopNav';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <TopNav />
       <Routes /> 
     </div>
   );
