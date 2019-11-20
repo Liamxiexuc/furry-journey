a25bee902b8d5469d8b8e4527ce8aec278f35c38
JimHui12 <jianghua.hui@uq.net.au>
2019-11-20 11:46:20 +1000

    TopNav.js and Routes.js updated

--- src/navigation/TopNav.js
+++ src/navigation/TopNav.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const TopNav = () => {
     return (
         <div>
-            <Link to="/manager/items">Menu</Link>
+            <Link to="/manager/menu">Menu</Link>
             <Link to="/manager/orders">Orders</Link>
         </div>
     )
--- src/route/Routes.js
+++ src/route/Routes.js
@@ -1,16 +1,19 @@
 import React, {Fragment} from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 
-import Menu from '../manager/items/Menu';
 import Orders from '../manager/orders/Orders';
+import OrderCard from '../manager/orders/components/OrderCard'
+import Items from '../manager/menu/Items';
 
 const Routes = () => {
     return (
         <Fragment>
             <Switch>
-                <Redirect exact from="/" to="/manager/items" />
-                <Route exact path="/manager/items" component={ Menu } />
+                <Redirect exact from="/" to="/manager/menu" />
+                <Route exact path="/manager/menu" component={ Items } />
+
                 <Route exact path="/manager/orders" component={ Orders } />
+                <Route exact path="/manager/orders/:id" component={ OrderCard } />
             </Switch>
         </Fragment>
     )
