0a029a8cc27705ad5ac7d92bba33ea4dec0c5a30
JimHui12 <jianghua.hui@uq.net.au>
2019-11-20 11:49:48 +1000

    Items.js updated

---
+++ src/manager/menu/Items.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ItemCard from './components/ItemCard';
+import './styles/item.scss';
+
+const Items = () => {
+    return (
+        <div>
+           <ItemCard /> 
+           <ItemCard /> 
+        </div>
+    )   
+};
+
+export default Items;
