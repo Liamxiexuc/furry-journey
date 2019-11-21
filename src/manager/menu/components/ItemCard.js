b2bef6813d6c8d0148880b63dcfffd290e410adf
JimHui12 <jianghua.hui@uq.net.au>
2019-11-22 08:52:32 +1000

    ItemCard.js and OrderCard.js updated

--- src/manager/menu/components/ItemCard.js
+++ src/manager/menu/components/ItemCard.js
@@ -2,7 +2,33 @@ import React from 'react';
 
 const ItemCard = props => {
     return (
-        <div className="item-card">dish 1</div>
+        <div className="item-card">
+            <div className="item-card--header">
+                <div className="item-name">
+                    Beef and Bacon pizza
+                </div>
+                <div className="item-price">
+                    15
+                </div>
+                <div className="item-photo">
+                    image
+                </div>
+            </div>
+            <div className="item-card--body">
+                <div className="item-type">
+                    Classic
+                </div>
+                <div className="item-info">
+                Huge Pie cut into 8 extra-large slices. 
+                Authentic soft and foldable New York Style dough 
+                topped with garlic sauce, cherry tomatoes, 
+                creamy mozzarella and oregano
+                </div>
+                <div className="item-category">
+                    Food
+                </div>
+            </div>
+        </div>
     )    
 };
 
--- src/manager/orders/components/OrderCard.js
+++ src/manager/orders/components/OrderCard.js
@@ -2,7 +2,35 @@ import React from 'react';
 
 const OrderCard = props => {
     return (
-        <div>order 1</div>
+        <div className="order-card">
+            <div className="order-card--header">
+                <div className="order-receiver-name">
+                    Client
+                </div>
+                <div className="order-receiver-phone">
+                    040 000 0000
+                </div>
+                <div className="order-receiver-address">
+                    1 Brisbane Street, Brisbane, 4000, QLD
+                </div>
+            </div>
+            <div className="order-card--body">
+                <div className="order-status">
+                    completed
+                </div>
+                <div className="order-total-price">
+                    14
+                </div>
+                <div className="order-pay-status">
+                    completed
+                </div>
+            </div>
+            <div className="order-card-footer">
+                <div className="order-comment">
+                    be quick
+                </div>
+            </div>
+        </div>
     )    
 };
 
