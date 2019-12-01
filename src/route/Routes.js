b226ade161e928bef845836df7ff8d86defd10ab
JimHui12 <jianghua.hui@uq.net.au>
2019-12-02 09:09:33 +1000

    Routes.js and URLMap.js updated

--- src/route/Routes.js
+++ src/route/Routes.js
@@ -5,15 +5,27 @@ import Orders from '../manager/orders/Orders';
 import OrderCard from '../manager/orders/components/OrderCard'
 import Items from '../manager/menu/Items';
 
+import {
+    ITEM_BASE_URL,
+    ORDER_BASE_URL,
+    SHOP_BASE_URL,
+    USER_BASE_URL
+} from './URLMap';
+
 const Routes = () => {
     return (
         <Fragment>
             <Switch>
                 <Redirect exact from="/" to="/manager/menu" />
                 <Route exact path="/manager/menu" component={ Items } />
+                <Route exact path="/manager/menu/new" component={ ItemNew } />
+                <Route exact path="/manager/menu/:id" component={ ItemDetails } />
+                <Route exact path="/manager/menu/:id/edit" component={ ItemEdit } />
+
 
                 <Route exact path="/manager/orders" component={ Orders } />
                 <Route exact path="/manager/orders/:id" component={ OrderCard } />
+           
             </Switch>
         </Fragment>
     )
