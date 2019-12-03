c78f7e2fdccbde5a66f25ea539839e8de8209fcf
JimHui12 <jianghua.hui@uq.net.au>
2019-12-03 23:16:07 +1000

    OrderInfo.js, ItemManagement.js, UserMangement.js

--- src/manager/orders/components/OrderInfo.js
+++ src/manager/orders/components/OrderInfo.js
@@ -3,5 +3,19 @@ import React, {useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router';
 
+import ItemManagement from './ItemManagement';
+import UserManagement from './UserManagement';
+
 import { ORDER_BASE_URL } from '../../../route/URLMap';
-import { deleteOrderById } from '../../../utils/api/item'
+
+import { deleteOrderById } from '../../../utils/api/item';
+
+const OrderInfo = props => {
+    const [ isDeleting, setIsDeleting ] = useState(false);
+
+    const {
+
+        
+    }
+
+}
