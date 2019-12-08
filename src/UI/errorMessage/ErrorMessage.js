2d11a027716556b271fd723509e4e3860b85f8b0
unknown <liachenxiexu@gmail.com>
2019-12-09 02:10:35 +1100

    navigation bar done

--- src/UI/errorMessage/ErrorMessage.js
+++ src/UI/errorMessage/ErrorMessage.js
@@ -2,11 +2,15 @@ import React from "react";
 import { Message, Icon } from "semantic-ui-react";
 
 export const ErrorMessage = ({ error }) => (
-  <Message icon error hidden={!error}>
-    <Icon name="ambulance" />
-    <Message.Header>Oops, Something goes wrong</Message.Header>
-    <p>{error && error.message}</p>
-  </Message>
-);
+         <Message icon error hidden={!error} size="large">
+           <Icon name="ambulance" />
+           <Message.Content>
+           <Message.Header>
+             Oops, Something goes wrong
+           </Message.Header>
+           {error && error.message}
+           </Message.Content>
+         </Message>
+       );
 
 //export default ErrorMessage;
--- src/client/navigation/TopNav.js
+++ src/client/navigation/TopNav.js
@@ -3,7 +3,7 @@ import { NavLink, withRouter } from "react-router-dom";
 import "./styles/topNav.scss";
 import { Button } from "semantic-ui-react";
 import { Navbar, Nav } from "react-bootstrap";
-import { deleteToken, isAuthenticated } from '../../utils/authentication';
+import { deleteToken, isAuthenticated } from "../../utils/authentication";
 import {
   LOGIN_URL,
   USER_BASE_URL,
@@ -18,21 +18,46 @@ const logout = history => {
 
 const TopNav = ({ history }) => {
   return (
-    <Navbar bg="light" variant="light">
-      <Navbar.Brand href="/home">Navbar</Navbar.Brand>
-      <Nav className="mr-auto">
-        <Nav.Link href="/home">Home</Nav.Link>
-        <Nav.Link href="/items">Items</Nav.Link>
-        <Nav.Link href="/member">Member</Nav.Link>
-
-        {isAuthenticated() && (
-          <Button onClick={() => logout(history)} className="nav-logout">
-            Log out
-          </Button>
-        )}
-      </Nav>
-    </Navbar>
+    <nav className="nav-bar">
+      <div className="nav-left">
+        <a className="nav-left_logo" href={HOME_BASE_URL}>
+          <img src="logo.png" alt="logo" />
+        </a>
+        <NavLink className="nav-left_brand" to={HOME_BASE_URL}>
+          NewBee Pizza
+        </NavLink>
+      </div>
+      <ul className="nav-item">
+        <NavLink className="nav-item" to={HOME_BASE_URL}>
+          Home
+        </NavLink>
+        <NavLink className="nav-item" to={USER_BASE_URL}>
+          Member
+        </NavLink>
+        <NavLink className="nav-item" to={ITEM_BASE_URL}>
+          Menu
+        </NavLink>
+        <NavLink className="nav-item" to={ITEM_BASE_URL}>
+          About us
+        </NavLink>
+      </ul>
+      <Button
+        size="huge"
+        compact
+        positive
+        onClick={() => logout(history)}
+        className="nav-logout"
+      >
+        MY ACCOUNT
+      </Button>
+    </nav>
   );
 };
 
 export default withRouter(TopNav);
+
+/*   {isAuthenticated() && (
+          <Button onClick={() => logout(history)} className="nav-logout">
+            Sign out
+          </Button>
+        )} */
