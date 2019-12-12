87e08553229fcd6173433116c57150e6fbf8fb87
JimHui12 <jianghua.hui@uq.net.au>
2019-12-12 12:10:07 +1000

    ItemCard.js updated

--- src/manager/menu/components/ItemCard.js
+++ src/manager/menu/components/ItemCard.js
@@ -31,23 +31,6 @@ const ItemCard = props => {
             </Card.Content>
         </Card>
         
-        // <div className="card">
-        //     <img src={avatar} />
-        //     <div className="container">
-        //         <div className="header">
-        //             <h4><b>{props.itemName}</b></h4>
-        //         </div>
-        //         <div className="body">
-        //             <div>{props.description}</div>
-        //         </div>
-                
-                
-        //         <p>
-        //             <Link to={`/items/detail/${item.id}`}>Details</Link>
-        //         </p>
-        //     </div>
-        // </div>
-        
     );    
 };
 
